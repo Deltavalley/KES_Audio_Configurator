572fb104e856eefb6f71144454a881839e1f0d02
Deltavalley-Laptop <lovelessjam@gmail.com>
2022-09-14 15:54:52 +0100

    wpf migration - ui & track adding done

---
+++ wpf/wpf/Common.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf
+{
+    public class AudioFile
+    {
+        public string DisplayName { get; set; }
+        public string FileName { get; }
+        public string FileExt { get; }
+        public bool Renameable { get; }
+
+        //ctor
+        public AudioFile(string displayName, string fileName, string fileExt, bool renameable)
+        {
+            DisplayName = displayName;
+            FileName = fileName;
+            FileExt = fileExt;
+            Renameable = renameable;
+        }
+
+        //ctor that assumes DisplayName = FileName
+        public AudioFile(string fileName, string fileExt) : this(fileName, fileName, fileExt, true) { }
+
+        //ctor w/ no args --> pause
+        public AudioFile() : this("---PAUSE---", "", "", false) { }
+    }
+
+    public static class Common
+    {
+        public static string errorEnd = $"{Environment.NewLine}If you don't know what this means, contact senior tech team";
+    }
+}
---
+++ wpf/wpf/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public OpenFileDialog OfdAudio;
+        const string imports = @"./imports";
+        public MainWindow()
+        {
+            InitializeComponent();
+            OfdAudio = new();
+            OfdAudio.FileOk += OfdAudio_FileOk;
+        }
+
+        private void OfdAudio_FileOk(object? sender, CancelEventArgs e)
+        {
+            //create ./imports if it doesnt already exist
+            if (!Directory.Exists(imports)) { Directory.CreateDirectory(imports); }
+
+            //copy files to ./imports
+            for (int i = 0; i < OfdAudio.FileNames.Length; i++)
+            {
+                //copy if the file isnt already there
+                if (!File.Exists($"./{imports}/{OfdAudio.SafeFileNames[i]}"))
+                {
+                    File.Copy(OfdAudio.FileNames[i], $"./{imports}/{OfdAudio.SafeFileNames[i]}");
+                }
+            }
+
+            //add the files to the list box 
+            foreach (string fileName in OfdAudio.SafeFileNames)
+            {
+                AudioFile currentAudio = new(fileName, GetExtension(fileName));
+                lstTracks.Items.Add(currentAudio);
+            }
+        }
+
+        private string GetExtension(string fileName)
+        {
+            return fileName.Substring(fileName.LastIndexOf(".") + 1);
+        }
+
+        private void btnAddAudio_Click(object sender, RoutedEventArgs e)
+        {
+            OfdAudio.ShowDialog();
+        }
+    }
+}
